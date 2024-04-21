e16b34fbf57809b6901752b5bbb1a3695b4485cc
jihunp <edwardpark.park@gmail.com>
2024-04-21 01:39:27 -0700

    creating a new navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Home from './pages/Home'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Home></Home>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar"
+
+function Home() {
+
+
+  return (
+    <>
+    <NavBar/>
+    this is the home page
+    </>
+  )
+}
+
+export default Home
