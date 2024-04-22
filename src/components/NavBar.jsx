6a40eb68f13ac0bbc28b7a0ed840ef3ad0a6a558
jihunp <edwardpark.park@gmail.com>
2024-04-22 00:34:40 -0700

    added a circle background to the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,13 +4,15 @@ import "../../public/styles/NavBar.css"
 
 function NavBar() {
   return (
-    <nav className='fixed-nav m-none flex inline-block align-center'>
-      <ul className='m-none flex li-none'>
-        <li className='p-right-10'><a href="/">Home</a></li>
-        <li className='p-right-10'><a href="/about">About</a></li>
-        <li className='p-right-10'><a href="/services">Services</a></li>
-        <li className='p-right-10'><a href="/contact">Contact</a></li>
-      </ul>
+    <nav className='navbar'>
+      <div className='navbar__circle'>
+        <ul className='navbar__menu'>
+          <li className='navbar__item'><a className='navbar__link' href="/">Home</a></li>
+          <li className='navbar__item'><a className='navbar__link' href="/about">About</a></li>
+          <li className='navbar__item'><a className='navbar__link' href="/services">Services</a></li>
+          <li className='navbar__item'><a className='navbar__link' href="/contact">Contact</a></li>
+        </ul>
+      </div>
     </nav>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 // import NavBar from "../components/NavBar"
-import VerticalNav from "../components/VerticalNav"
-
+// import VerticalNav from "../components/VerticalNav"
+import NavBar from "../components/NavBar"
 function Home() {
 
 
   return (
     <>
-    <VerticalNav/>
-    {/* <NavBar/> */}
+    {/* <VerticalNav/> */}
+    <NavBar/>
     </>
   )
 }
