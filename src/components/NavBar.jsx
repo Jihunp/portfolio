4e046a8a59059e43bac597408b7ee6263d85ad47
jihunp <edwardpark.park@gmail.com>
2024-04-21 22:25:38 -0700

    created new simple nav bar and starting a styled vertical nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import "../../public/styles/NavBar.css"
+
 
 function NavBar() {
   return (
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/services">Services</a></li>
-        <li><a href="/contact">Contact</a></li>
+    <nav className='fixed-nav m-none flex inline-block align-center'>
+      <ul className='m-none flex li-none'>
+        <li className='p-right-10'><a href="/">Home</a></li>
+        <li className='p-right-10'><a href="/about">About</a></li>
+        <li className='p-right-10'><a href="/services">Services</a></li>
+        <li className='p-right-10'><a href="/contact">Contact</a></li>
       </ul>
     </nav>
   );
